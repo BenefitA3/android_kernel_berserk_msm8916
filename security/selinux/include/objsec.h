09da030cd9f12a9d8c1653336b8653e0d03cf117
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-08-27 16:09:49 +0600

    update

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -25,9 +25,6 @@
 #include <linux/in.h>
 #include <linux/spinlock.h>
 #include <linux/in6.h>
-#include <net/net_namespace.h>
-#include "flask.h"
-#include "avc.h"
 
 struct task_security_struct {
 	u32 osid;		/* SID prior to last execve */
@@ -81,7 +78,6 @@ struct ipc_security_struct {
 };
 
 struct netif_security_struct {
-	struct net *ns;			/* network namespace */
 	int ifindex;			/* device index */
 	u32 sid;			/* SID for this interface */
 };
